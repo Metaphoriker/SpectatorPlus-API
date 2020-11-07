25d6ba043ecfd622caf419a9763e78f046ec717f
Luziferium <luziferisdoof@gmail.com>
2020-11-08 00:26:10 +0100

    Changes

--- src/main/java/de/luzifer/spectator/api/SPApi.java
+++ src/main/java/de/luzifer/spectator/api/SPApi.java
@@ -1,6 +1,5 @@
 package de.luzifer.spectator.api;
 
-import de.luzifer.spectator.SpectatorPlus;
 import de.luzifer.spectator.api.manager.CameraManager;
 import de.luzifer.spectator.api.manager.SpectateModeManager;
 import de.luzifer.spectator.api.manager.SpectatorManager;
@@ -10,13 +9,17 @@ import de.luzifer.spectator.api.manager.SpectatorManager;
  */
 public class SPApi {
 
+    static CameraManager cameraManager;
+    static SpectatorManager spectatorManager;
+    static SpectateModeManager spectateModeManager;
+
     /**
      * Returns the CameraManager
      *
      * @return the CameraManager to manage/create (new) Cameras
      */
     public static CameraManager getCameraManager() {
-        return SpectatorPlus.cameraManager;
+        return cameraManager;
     }
 
     /**
@@ -25,7 +28,7 @@ public class SPApi {
      * @return the SpectatorManager to manage Spectators
      */
     public static SpectatorManager getSpectatorManager() {
-        return SpectatorPlus.spectatorManager;
+        return spectatorManager;
     }
 
     /**
@@ -33,5 +36,17 @@ public class SPApi {
      *
      * @return the SpectateModeManager to manage SpectateModes
      */
-    public static SpectateModeManager getSpectateModeManager() {return SpectatorPlus.spectateModeManager;}
+    public static SpectateModeManager getSpectateModeManager() {return spectateModeManager;}
+
+    public void setCameraManager(CameraManager cameraManager) {
+        SPApi.cameraManager = cameraManager;
+    }
+
+    public void setSpectatorManager(SpectatorManager spectatorManager) {
+        SPApi.spectatorManager = spectatorManager;
+    }
+
+    public void setSpectateModeManager(SpectateModeManager spectateModeManager) {
+        SPApi.spectateModeManager = spectateModeManager;
+    }
 }
